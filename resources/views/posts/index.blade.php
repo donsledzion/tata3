37ca9886af9d0f228b8a1433f4a3f584ea384bb6
donsledzion <adam@ulinia8.pl>
2021-07-27 11:13:34 +0200

    testing cloned repo

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,13 +7,16 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 inline animate-pulse" fill="none" viewBox="0 0 24 24" stroke="green">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 inline animate-pulse" fill="none" viewBox="0 0 24 24" stroke="green">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
                 </a>
             </h2>
         </x-slot>
 
         <!-- component -->
 
-            <input type="text" id="feeds-param" name="feeds-param" value="@if($kid){{$kid->id}}@endif" >
+            <input type="text" id="feeds-param" name="feeds-param" value="@if($kid){{$kid->id}}@endif" hidden>
 
         <div class="table w-full p-2">
         <!-- -->
